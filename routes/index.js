bace394f1814222b078eb99a836fbd09bb17601c
Kenya Peterson <kenyap013@gmail.com>
2025-04-17 13:36:01 -0400

    fix constant

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const clothingItem = require('./clothingItem');
 const userRouter = require('./users');
+const { NOT_FOUND } = require('../utils/errors');
 
 router.use('/users', userRouter);
 router.use('/items', clothingItem);
 
 router.use((req, res) => {
-  res.status(404).send({ message: 'Requested resource not found' });
+  res.status(NOT_FOUND).send({ message: 'Requested resource not found' });
 }
 );
 
