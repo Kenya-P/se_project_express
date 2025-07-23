3bbd3df03208e747baf64aee9fba5ce0644b379e
Kenya Peterson <kenyap013@gmail.com>
2025-07-22 21:12:58 -0400

    fix errors

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ app.use(errors());
 app.use(errorHandler);
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
